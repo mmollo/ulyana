59750f4caff421b156d9b805e98810076b9f1f5f
Mallory Mollo <mallory.mollo@digitick.com>
2017-07-20 18:31:29 +0200

    Adds a static file server

--- main.go
+++ main.go
@@ -1,25 +1,41 @@
 package main
 
 import (
+	"flag"
+	"github.com/nu7hatch/gouuid"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
 	"os"
-	"time"
 )
 
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 50
 
-func main() {
-	http.HandleFunc("/upload", upload)
+var ud *string
 
-	err := http.ListenAndServe(":8080", nil)
+func main() {
+
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	ud = flag.String("d", cwd+"/html/upload", "Upload directory")
+	flag.Parse()
+
+	http.Handle("/", http.FileServer(http.Dir("html")))
+	http.HandleFunc("/upload", handler_upload)
+
+	err = http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-func upload(w http.ResponseWriter, r *http.Request) {
+func handler_upload(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" && r.Method != "PUT" {
+		return
+	}
 	r.Body = http.MaxBytesReader(w, r.Body, MAX_UPLOAD_SIZE)
 	err := r.ParseMultipartForm(MAX_UPLOAD_SIZE)
 
@@ -29,25 +45,58 @@ func upload(w http.ResponseWriter, r *http.Request) {
 	}
 
 	file, handler, err := r.FormFile("file")
+	mimetype := handler.Header.Get("Content-Type")
 
-	defer file.Close()
+	switch mimetype {
+	case "image/jpeg":
+	case "image/png":
+	case "image/gif":
+		break
+	default:
+		return
+	}
+
 	if err != nil {
 		log.Println(err)
 		return
 	}
+	defer file.Close()
 
-	f, err := os.OpenFile("./up/"+handler.Filename+time.Now().String(), os.O_WRONLY|os.O_CREATE, 0666)
+	uid := upload(file)
+	if r.Method == "PUT" {
+		w.Write([]byte(uid))
+		return
+	}
+
+	if r.Method == "POST" {
+		w.Header().Set("Location", "/upload/"+uid+".jpg")
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+}
+
+func upload(file multipart.File) string {
+	u4, err := uuid.NewV4()
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+
+	filename := u4.String()
+	f, err := os.OpenFile(*ud+"/"+filename+".jpg", os.O_WRONLY|os.O_CREATE, 0666)
 	defer f.Close()
 
 	if err != nil {
 		log.Println(err)
-		return
+		return ""
 	}
 
 	_, err = io.Copy(f, file)
 
 	if err != nil {
 		log.Println(err)
-		return
+		return ""
 	}
+
+	return filename
 }
